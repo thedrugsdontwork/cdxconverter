8ff695f987ff7d450da622c1b49488e032cdddd2
yaojunhao <yaojunhao@stonewise.cn>
2022-10-26 12:04:07 +0800

    fix the processing logic of mol file fragments

--- test/test.py
+++ test/test.py
@@ -1,7 +1,13 @@
+import os
+import sys
+root="/".join(os.path.abspath(__file__).split("/")[:-2])
+print(root)
+sys.path.append(root)
 from io import StringIO
 from cdxconverter.convert import cdx_file_to_mol
 from rdkit.Chem import Draw
 from rdkit import Chem
+import time
 if __name__=='__main__':
     file = "./target.cdx"
     lis=cdx_file_to_mol(file)
@@ -9,8 +15,8 @@ if __name__=='__main__':
     with open("./test.mol", "wt", encoding="utf-8") as fp:
         fp.write(Chem.MolToMolBlock(rmol))
     mol=Chem.MolFromMolFile("./test.mol")
-
-    Draw.MolToFile(mol, "./test.png",size=(300,300))
+    # time.sleep(1)
+    Draw.MolToFile(rmol, "./test.png",size=(500,500))
 
     with open("./test.smiles","wt",encoding="utf-8") as fp:
         fp.write(Chem.MolToSmiles(mol))
@@ -18,4 +24,4 @@ if __name__=='__main__':
     print(Chem.MolToMolBlock(mol))
     print(Chem.MolToSmiles(mol))
     targetMol=Chem.MolFromMolFile("./target.mol")
-    print(f"Chem.MolToSmiles(mol)==Chem.MolToSmiles(targetMol):{Chem.MolToSmiles(mol)==Chem.MolToSmiles(targetMol)}")
+    print(f"{Chem.MolToSmiles(mol)}=={Chem.MolToSmiles(targetMol)}:{Chem.MolToSmiles(mol)==Chem.MolToSmiles(targetMol)}")
